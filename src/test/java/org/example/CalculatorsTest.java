b97ec27e78d1d26df3a73182c6d3fe68e990222f
Shivakant6387 <shivakant5875@gmail.com>
2023-05-15 14:37:40 +0530

    @before All method

---
+++ src/test/java/org/example/CalculatorsTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorsTest {
+    private static ICalculator calculator;
+    @BeforeAll
+    static void test_SuccessFully_Start(){
+        System.out.println("Test Started");
+    }
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void divison() {
+    }
+
+    @Test
+    void equalIntegers() {
+    }
+}
